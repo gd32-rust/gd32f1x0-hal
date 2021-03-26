72a3210b8d470ba6e8d07e252272d32754f3e4c3
Andrew Walbran <qwandor@google.com>
2021-03-26 20:44:39 +0000

    Add split, TX-only and RX-only modes, and implement fmt::Write.

--- src/serial.rs
+++ src/serial.rs
@@ -3,6 +3,7 @@
 //! This module contains the functions to utilize the USART (Universal
 //! synchronous asynchronous receiver transmitter).
 
+use core::fmt;
 use core::marker::PhantomData;
 use core::ops::Deref;
 use core::ptr;
@@ -103,14 +104,16 @@ pub struct Serial<USART, TXPIN, RXPIN> {
 
 /// Serial receiver
 pub struct Rx<USART> {
-    _usart: PhantomData<USART>,
+    usart: *const usart0::RegisterBlock,
+    _instance: PhantomData<USART>,
 }
 
 unsafe impl<USART> Send for Rx<USART> {}
 
 /// Serial transmitter
 pub struct Tx<USART> {
-    _usart: PhantomData<USART>,
+    usart: *const usart0::RegisterBlock,
+    _instance: PhantomData<USART>,
 }
 
 unsafe impl<USART> Send for Tx<USART> {}
@@ -124,35 +127,7 @@ impl<TXPIN: TxPin<USART0>, RXPIN: RxPin<USART0>> Serial<USART0, TXPIN, RXPIN> {
         clocks: Clocks,
         apb: &mut APB2,
     ) -> Self {
-        // Enable clock for USART, and reset it.
-        USART0::enable(apb);
-        USART0::reset(apb);
-
-        // Configure baud rate.
-        let baud_rate_ratio = <USART0 as RcuBus>::Bus::get_frequency(&clocks).0 / config.baudrate.0;
-        assert!(baud_rate_ratio >= 16 && baud_rate_ratio <= 0xFFFF);
-        usart.baud.write(|w| unsafe { w.bits(baud_rate_ratio) });
-
-        // Configure parity. Note that the parity bit counts towards the word length, so we have to
-        // increase it to 9 bits if parity is enabled so as to still get 8 data bits.
-        match config.parity {
-            Parity::ParityNone => {
-                usart.ctl0.modify(|_, w| w.pcen().disabled().wl().bit8());
-            }
-            Parity::ParityEven => {
-                usart
-                    .ctl0
-                    .modify(|_, w| w.pcen().enabled().wl().bit9().pm().even());
-            }
-            Parity::ParityOdd => {
-                usart
-                    .ctl0
-                    .modify(|_, w| w.pcen().enabled().wl().bit9().pm().odd());
-            }
-        }
-
-        // Configure stop bits.
-        usart.ctl1.modify(|_, w| w.stb().variant(config.stopbits));
+        usart.enable_configure(config, clocks, apb);
 
         // Enable transmitter, receiver and the USART as a whole.
         usart
@@ -163,13 +138,206 @@ impl<TXPIN: TxPin<USART0>, RXPIN: RxPin<USART0>> Serial<USART0, TXPIN, RXPIN> {
     }
 }
 
+impl<TXPIN: TxPin<USART0>> Serial<USART0, TXPIN, ()> {
+    /// Configures the USART and creates a new TX-only Serial instance.
+    pub fn usart0tx(
+        usart: USART0,
+        txpin: TXPIN,
+        config: Config,
+        clocks: Clocks,
+        apb: &mut APB2,
+    ) -> Self {
+        usart.enable_configure(config, clocks, apb);
+
+        // Enable transmitter and the USART as a whole.
+        usart.ctl0.modify(|_, w| w.ten().enabled().uen().enabled());
+
+        Self {
+            usart,
+            pins: (txpin, ()),
+        }
+    }
+}
+
+impl<RXPIN: RxPin<USART0>> Serial<USART0, (), RXPIN> {
+    /// Configures the USART and creates a new RX-only Serial instance.
+    pub fn usart0tx(
+        usart: USART0,
+        rxpin: RXPIN,
+        config: Config,
+        clocks: Clocks,
+        apb: &mut APB2,
+    ) -> Self {
+        usart.enable_configure(config, clocks, apb);
+
+        // Enable receiver and the USART as a whole.
+        usart.ctl0.modify(|_, w| w.ren().enabled().uen().enabled());
+
+        Self {
+            usart,
+            pins: ((), rxpin),
+        }
+    }
+}
+
+impl<USART: Deref<Target = usart0::RegisterBlock>, TXPIN: TxPin<USART0>, RXPIN: RxPin<USART0>>
+    Serial<USART, TXPIN, RXPIN>
+{
+    /// Separates the serial struct into separate channel objects for sending (Tx) and receiving (Rx).
+    pub fn split(self) -> (Tx<USART>, Rx<USART>) {
+        (
+            Tx {
+                usart: &*self.usart,
+                _instance: PhantomData,
+            },
+            Rx {
+                usart: &*self.usart,
+                _instance: PhantomData,
+            },
+        )
+    }
+}
+
+impl<USART: Deref<Target = usart0::RegisterBlock>, TXPIN, RXPIN> Serial<USART, TXPIN, RXPIN> {
+    pub fn release(self) -> (USART, (TXPIN, RXPIN)) {
+        (self.usart, self.pins)
+    }
+}
+
+// Implement writing traits if the USART has a TX pin assigned.
 impl<USART: Deref<Target = usart0::RegisterBlock>, TXPIN: TxPin<USART>, RXPIN> Write<u8>
     for Serial<USART, TXPIN, RXPIN>
 {
     type Error = Infallible;
 
     fn flush(&mut self) -> nb::Result<(), Self::Error> {
-        let status = self.usart.stat.read();
+        self.usart.flush()
+    }
+
+    fn write(&mut self, byte: u8) -> nb::Result<(), Self::Error> {
+        self.usart.write(byte)
+    }
+}
+
+impl<USART: Deref<Target = usart0::RegisterBlock>> Write<u8> for Tx<USART> {
+    type Error = Infallible;
+
+    fn flush(&mut self) -> nb::Result<(), Self::Error> {
+        unsafe { &*self.usart }.flush()
+    }
+
+    fn write(&mut self, byte: u8) -> nb::Result<(), Self::Error> {
+        unsafe { &*self.usart }.write(byte)
+    }
+}
+
+impl<USART: Deref<Target = usart0::RegisterBlock>, TXPIN: TxPin<USART>, RXPIN> fmt::Write
+    for Serial<USART, TXPIN, RXPIN>
+{
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        s.as_bytes()
+            .iter()
+            .try_for_each(|c| nb::block!(self.write(*c)))
+            .map_err(|_| core::fmt::Error)
+    }
+}
+
+impl<USART: Deref<Target = usart0::RegisterBlock>> fmt::Write for Tx<USART> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        s.as_bytes()
+            .iter()
+            .try_for_each(|c| nb::block!(self.write(*c)))
+            .map_err(|_| core::fmt::Error)
+    }
+}
+
+// Implement readin trait if the USART has an RX pin assigned.
+impl<USART: Deref<Target = usart0::RegisterBlock>, TXPIN, RXPIN: RxPin<USART>> Read<u8>
+    for Serial<USART, TXPIN, RXPIN>
+{
+    type Error = Error;
+
+    fn read(&mut self) -> nb::Result<u8, Error> {
+        self.usart.read()
+    }
+}
+
+impl<USART: Deref<Target = usart0::RegisterBlock>> Read<u8> for Rx<USART> {
+    type Error = Error;
+
+    fn read(&mut self) -> nb::Result<u8, Error> {
+        unsafe { &*self.usart }.read()
+    }
+}
+
+trait UsartConfigExt {
+    fn enable_configure(&self, config: Config, clocks: Clocks, apb: &mut APB2);
+}
+
+impl<USART: RcuBus + Deref<Target = usart0::RegisterBlock>> UsartConfigExt for USART {
+    /// Enable, reset and configure the USART.
+    fn enable_configure(&self, config: Config, clocks: Clocks, apb: &mut APB2) {
+        // Enable clock for USART, and reset it.
+        USART0::enable(apb);
+        USART0::reset(apb);
+
+        // Configure baud rate.
+        let baud_rate_ratio = <USART0 as RcuBus>::Bus::get_frequency(&clocks).0 / config.baudrate.0;
+        assert!(baud_rate_ratio >= 16 && baud_rate_ratio <= 0xFFFF);
+        self.baud.write(|w| unsafe { w.bits(baud_rate_ratio) });
+
+        // Configure parity. Note that the parity bit counts towards the word length, so we have to
+        // increase it to 9 bits if parity is enabled so as to still get 8 data bits.
+        match config.parity {
+            Parity::ParityNone => {
+                self.ctl0.modify(|_, w| w.pcen().disabled().wl().bit8());
+            }
+            Parity::ParityEven => {
+                self.ctl0
+                    .modify(|_, w| w.pcen().enabled().wl().bit9().pm().even());
+            }
+            Parity::ParityOdd => {
+                self.ctl0
+                    .modify(|_, w| w.pcen().enabled().wl().bit9().pm().odd());
+            }
+        }
+
+        // Configure stop bits.
+        self.ctl1.modify(|_, w| w.stb().variant(config.stopbits));
+    }
+}
+
+trait UsartReadWrite {
+    fn read(&mut self) -> nb::Result<u8, Error>;
+    fn flush(&mut self) -> nb::Result<(), Infallible>;
+    fn write(&mut self, byte: u8) -> nb::Result<(), Infallible>;
+}
+
+impl<USART: Deref<Target = usart0::RegisterBlock>> UsartReadWrite for USART {
+    fn read(&mut self) -> nb::Result<u8, Error> {
+        let status = self.stat.read();
+
+        if status.perr().bit_is_set() {
+            self.intc.write(|w| w.pec().clear());
+            Err(nb::Error::Other(Error::Parity))
+        } else if status.ferr().bit_is_set() {
+            self.intc.write(|w| w.fec().clear());
+            Err(nb::Error::Other(Error::Framing))
+        } else if status.nerr().bit_is_set() {
+            self.intc.write(|w| w.nec().clear());
+            Err(nb::Error::Other(Error::Noise))
+        } else if status.orerr().bit_is_set() {
+            self.intc.write(|w| w.orec().clear());
+            Err(nb::Error::Other(Error::Overrun))
+        } else if status.rbne().bit_is_set() {
+            Ok(self.rdata.read().rdata().bits() as u8)
+        } else {
+            Err(nb::Error::WouldBlock)
+        }
+    }
+
+    fn flush(&mut self) -> nb::Result<(), Infallible> {
+        let status = self.stat.read();
         if status.tc().bit_is_set() {
             Ok(())
         } else {
@@ -177,41 +345,11 @@ impl<USART: Deref<Target = usart0::RegisterBlock>, TXPIN: TxPin<USART>, RXPIN> W
         }
     }
 
-    fn write(&mut self, byte: u8) -> nb::Result<(), Self::Error> {
-        let status = self.usart.stat.read();
+    fn write(&mut self, byte: u8) -> nb::Result<(), Infallible> {
+        let status = self.stat.read();
         if status.tbe().bit_is_set() {
-            self.usart
-                .tdata
-                .write(|w| unsafe { w.tdata().bits(byte.into()) });
+            self.tdata.write(|w| unsafe { w.tdata().bits(byte.into()) });
             Ok(())
-        } else {
-            Err(nb::Error::WouldBlock)
-        }
-    }
-}
-
-impl<USART: Deref<Target = usart0::RegisterBlock>, TXPIN, RXPIN: RxPin<USART>> Read<u8>
-    for Serial<USART, TXPIN, RXPIN>
-{
-    type Error = Error;
-
-    fn read(&mut self) -> nb::Result<u8, Error> {
-        let status = self.usart.stat.read();
-
-        if status.perr().bit_is_set() {
-            self.usart.intc.write(|w| w.pec().clear());
-            Err(nb::Error::Other(Error::Parity))
-        } else if status.ferr().bit_is_set() {
-            self.usart.intc.write(|w| w.fec().clear());
-            Err(nb::Error::Other(Error::Framing))
-        } else if status.nerr().bit_is_set() {
-            self.usart.intc.write(|w| w.nec().clear());
-            Err(nb::Error::Other(Error::Noise))
-        } else if status.orerr().bit_is_set() {
-            self.usart.intc.write(|w| w.orec().clear());
-            Err(nb::Error::Other(Error::Overrun))
-        } else if status.rbne().bit_is_set() {
-            Ok(self.usart.rdata.read().rdata().bits() as u8)
         } else {
             Err(nb::Error::WouldBlock)
         }
