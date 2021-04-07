de791310ef97ec30521e903a962c572e66a631d8
Andrew Walbran <qwandor@google.com>
2021-04-07 23:47:41 +0100

    WIP on EXTI example.

--- examples/exti.rs
+++ examples/exti.rs
@@ -10,18 +10,18 @@ use panic_halt as _;
 
 use core::mem::MaybeUninit;
 use cortex_m_rt::entry;
-use pac::interrupt;
-use stm32f1xx_hal::gpio::*;
-use stm32f1xx_hal::{pac, prelude::*};
+use gd32f1x0_hal::{
+    gpio::{gpioa::PA7, gpioc::PC13, Floating, Input, Output, PushPull},
+    pac::{self, interrupt},
+    prelude::*,
+};
 
 // These two are owned by the ISR. main() may only access them during the initialization phase,
 // where the interrupt is not yet enabled (i.e. no concurrent accesses can occur).
 // After enabling the interrupt, main() may not have any references to these objects any more.
 // For the sake of minimalism, we do not use RTIC here, which would be the better way.
-static mut LED: MaybeUninit<stm32f1xx_hal::gpio::gpioc::PC13<Output<PushPull>>> =
-    MaybeUninit::uninit();
-static mut INT_PIN: MaybeUninit<stm32f1xx_hal::gpio::gpioa::PA7<Input<Floating>>> =
-    MaybeUninit::uninit();
+static mut LED: MaybeUninit<PC13<Output<PushPull>>> = MaybeUninit::uninit();
+static mut INT_PIN: MaybeUninit<PA7<Input<Floating>>> = MaybeUninit::uninit();
 
 #[interrupt]
 fn EXTI9_5() {
@@ -44,17 +44,16 @@ fn main() -> ! {
     {
         // the scope ensures that the int_pin reference is dropped before the first ISR can be executed.
 
-        let mut rcc = p.RCC.constrain();
-        let mut gpioa = p.GPIOA.split(&mut rcc.apb2);
-        let mut gpioc = p.GPIOC.split(&mut rcc.apb2);
-        let mut afio = p.AFIO.constrain(&mut rcc.apb2);
+        let mut rcu = p.RCU.constrain();
+        let mut gpioa = p.GPIOA.split(&mut rcu.ahb);
+        let mut gpioc = p.GPIOC.split(&mut rcu.ahb);
 
         let led = unsafe { &mut *LED.as_mut_ptr() };
-        *led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+        *led = gpioc.pc13.into_push_pull_output(&mut gpioc.config);
 
         let int_pin = unsafe { &mut *INT_PIN.as_mut_ptr() };
-        *int_pin = gpioa.pa7.into_floating_input(&mut gpioa.crl);
-        int_pin.make_interrupt_source(&mut afio);
+        *int_pin = gpioa.pa7.into_floating_input(&mut gpioa.config);
+        int_pin.make_interrupt_source();
         int_pin.trigger_on_edge(&p.EXTI, Edge::RISING_FALLING);
         int_pin.enable_interrupt(&p.EXTI);
     } // initialization ends here
