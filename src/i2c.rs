4531fa35cd6022c34a27671ddcb93e4b52140fca
Andrew Walbran <qwandor@google.com>
2021-09-26 22:24:26 +0100

    Tidy up comment.

--- src/i2c.rs
+++ src/i2c.rs
@@ -1,7 +1,6 @@
 //! Inter-Integrated Circuit (I2C) bus
 
-// This document describes a correct i2c implementation and is what
-// parts of this code is based on
+// This document describes a correct I2C implementation and is what parts of this code is based on:
 // https://www.st.com/content/ccc/resource/technical/document/application_note/5d/ae/a3/6f/08/69/4e/9b/CD00209826.pdf/files/CD00209826.pdf/jcr:content/translations/en.CD00209826.pdf
 
 use crate::gpio::gpioa::*;
